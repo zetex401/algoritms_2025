9bb4dd90732fcd2e5196a5d8e0af9668c7d83938
zetex401 <zetex401@gmail.com>
2025-12-16 00:02:59 +0500

    НЕ МОГЛИ СОЗДАТЬ ФАЙЛ 3000000 ЛЕТ, НО СПУСТЯ ЭТО ВРЕМЯ, ПОЛУЧИЛОООООООСЬ!!!!

--- second_semestry_hw/solutions.py
+++ second_semestry_hw/solutions.py
@@ -1,5 +1,6 @@
 import time
 import random
+
 def find_common_naive(arr1, arr2):
     """
     Наивный подход: O(n * m)
@@ -20,5 +21,16 @@ def find_common_set(arr1, arr2):
     set2 = set(arr2)
     return list(set1 & set2)
 
-arr1 = [random.randint(1, n // 2) for _ in range(n)]
-arr2 = [random.randint(1, n // 2) for _ in range(n)]
+n = 50000
+arr1 = [random.randint(1, 30000) for _ in range(n)]
+arr2 = [random.randint(1, 30000) for _ in range(n)]
+
+#with open('resultshw01.txt', 'w', encoding='utf-8') as f:
+#with open('resultshw01.txt', 'w', encoding='utf-8') as f:
+#with open('./resultshw01.txt', 'w', encoding='utf-8') as f:
+import os
+script_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_dir, 'resultshw01.txt')
+with open(file_path, 'w', encoding='utf-8') as f:
+
+    f.write("ДОМАШНЕЕ ЗАДАНИЕ 1 - РЕЗУЛЬТАТЫ \n")
